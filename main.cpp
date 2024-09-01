7b6b135a464f0ee58bcaf0377e92f69604489c68
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-09-01 09:55:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main(){
 
 
       Hand my_hand;
-      my_hand.Add(10);
-      my_hand.Add(6);
-      test_map.Add(10);
-      test_map.Add(10);
-      test_map.Add(6);
-      BJ.Stats(test_map, my_hand, 10);
+      my_hand.Add(8);
+      my_hand.Add(8);
+      test_map.Add(8);
+      test_map.Add(8);
+      test_map.Add(8);
+      BJ.Stats(test_map, my_hand, 8);
 }
