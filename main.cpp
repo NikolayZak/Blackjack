1bd0d1d6ba78cfbf705b75e3de2e2330261fa37c
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-09-17 09:08:40 -0700

    garbage collection

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(){
       Absent_Map test_map(NUMBER_OF_DECKS);
       Computed_Strategy_Chart Chart;
       
-
       // For now: computing each chart of 1 card missing at a time
       for(int i = 1; i < 3; i++){
             auto start = chrono::system_clock::now();
@@ -25,43 +24,6 @@ int main(){
             cout << "Time Elapsed: " << elapsed_seconds.count() / 60 << " minutes " << elapsed_seconds.count() % 60 << " seconds\n";
       }
 
-
-
-
-
-
-      /* deal out and compute every permutation of 1 card missing
-      int max_cards = 3;
-      int next_card;
-      bool loop = true;
-      vector<int> stack;
-      stack.push_back(1);
-      while(loop){
-            if((int)stack.size() < max_cards){
-                  test_map.Add(stack.back());
-                  stack.push_back(stack.back());
-            }
-            if((int)stack.size() == max_cards){
-                  print(stack);
-                  Chart.Configure(test_map);
-                  Chart.Print_All();
-
-                  while(stack.back() == 10){
-                        test_map.Remove(10);
-                        stack.pop_back();
-                  }
-                  if(stack.size() == 0){
-                        loop = false;
-                        break;
-                  }
-                  next_card = stack.back() + 1;
-                  test_map.Remove(stack.back());
-                  test_map.Add(next_card);
-                  stack.pop_back();
-                  stack.push_back(next_card);
-            }
-      }
-      */
       
       return 0;
 }
