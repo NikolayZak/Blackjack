3c270d6da0b3ed1d7f8ff00beedbe4cf4721381b
NikolayZak <147001597+NikolayZak@users.noreply.github.com>
2024-09-19 01:30:34 -0700

    scalability

--- main.cpp
+++ main.cpp
@@ -2,26 +2,31 @@
 
 #define NUMBER_OF_DECKS 4
 
+#define NUMBER_OF_POOL_ADDS 3
+
+void compute_rec(Absent_Map current){
+      Computed_Strategy_Chart Chart;
+      if(current.Cards_Left() != NUMBER_OF_DECKS * 52 - NUMBER_OF_POOL_ADDS){
+            for(int i = 1; i < 11; i++){
+                  current.Add(i);
+                  compute_rec(current);
+                  current.Remove(i);
+            }
+      }
+      auto start = chrono::system_clock::now();
+
+      Chart.Configure(current);
+      Chart.Print_All();
+
+      auto end = chrono::system_clock::now();
+      auto elapsed_seconds = chrono::duration_cast<chrono::seconds>(end - start);
+      cout << "Time Elapsed: " << elapsed_seconds.count() / 60 << " minutes " << elapsed_seconds.count() % 60 << " seconds\n";
+}
+
+
 int main(){
       Absent_Map test_map(NUMBER_OF_DECKS);
-      Computed_Strategy_Chart Chart;
-      
-      // For now: computing each chart of 1 card missing at a time
-      for(int i = 1; i < 11; i++){
-            auto start = chrono::system_clock::now();
-
-            test_map.Add(i);
-
-            test_map.Print();
-            Chart.Configure(test_map);
-            Chart.Print_All();
-
-            test_map.Remove(i);
-
-            auto end = chrono::system_clock::now();
-            auto elapsed_seconds = chrono::duration_cast<chrono::seconds>(end - start);
-            cout << "Time Elapsed: " << elapsed_seconds.count() / 60 << " minutes " << elapsed_seconds.count() % 60 << " seconds\n";
-      }
+      compute_rec(test_map);
       
       return 0;
 }
